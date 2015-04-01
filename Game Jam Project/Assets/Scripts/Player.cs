0c98c72e15d52d72f0ccba731c28aef5209b1a68
Neucrotic <nelsonhain@gmail.com>
2015-04-01 15:04:32 +1100

    bler

--- Game Jam Project/Assets/Scripts/Player.cs
+++ Game Jam Project/Assets/Scripts/Player.cs
@@ -14,45 +14,77 @@ public class Player : MonoBehaviour
     Rigidbody physicsBody;
 
 	// Use this for initialization
-    void Start()
+    public void Start()
     {
+        up = KeyCode.W;
+        down = KeyCode.S;
+        left = KeyCode.A;
+        right = KeyCode.D;
         physicsBody = GetComponent<Rigidbody>();
     }
 
 	// Update is called once per frame
-    void Update()
+    public void Update()
     {
 
     }
     
-    void FixedUpdate() 
+    public void FixedUpdate() 
     {
         physicsBody.velocity = new Vector3(physicsBody.velocity.x * Friction, physicsBody.velocity.y, physicsBody.velocity.z * Friction);
 
+        Movement();
+	}
+
+    void Movement()
+    {
         Vector3 velocity = physicsBody.velocity;
 
-	    //input checks
-        if (Input.GetKey(KeyCode.W) == true && velocity.z < 1)
+
+
+        //input checks
+        if (Input.GetKey(up) == true && velocity.z < 1)
         {
             physicsBody.velocity += Vector3.forward;
             //Debug.Log("UP");
         }
-        if (Input.GetKey(KeyCode.S) == true && velocity.z > -1)
+        if (Input.GetKey(down) == true && velocity.z > -1)
         {
             physicsBody.velocity += Vector3.back;
             //Debug.Log("DOWN");
         }
-        if (Input.GetKey(KeyCode.A) == true && velocity.x > -1)
+        if (Input.GetKey(left) == true && velocity.x > -1)
         {
             physicsBody.velocity += Vector3.left;
             //Debug.Log("LEFT");
         }
-        if (Input.GetKey(KeyCode.D) == true && velocity.x < 1)
+        if (Input.GetKey(right) == true && velocity.x < 1)
         {
             physicsBody.velocity += Vector3.right;
-           //Debug.Log("RIGHT");
+            //Debug.Log("RIGHT");
         }
+        if (Input.GetKey(KeyCode.O) == true)
+        {
+            BasicAttack();
+        }
+        if (Input.GetKey(KeyCode.P) == true)
+        {
+            SpecialAttack();
+        }
+    }
 
+    void Combat()
+    {
         
-	}
+    }
+
+    void BasicAttack()
+    {
+        physicsBody.velocity += Vector3.forward * 5;
+    }
+    
+    void SpecialAttack()
+    {
+        physicsBody.velocity += Vector3.forward * 15;
+    }
 }
